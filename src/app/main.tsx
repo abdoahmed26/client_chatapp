3ae9884d47ac92081ffa3ecc2f5c1306e4fd35b7
Abdulrahman Ahmed <abdulrahman.ahmed2623@gmail.com>
2026-04-21 12:32:49 +0200

    fixed bug not found route

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -10,9 +10,9 @@ import { queryClient } from "@/lib/query-client";
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </Provider>
 );
